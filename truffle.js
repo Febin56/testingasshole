ab0884bca4e861b839db07820b03d1074f2d3265
febin <febin56@gmail.com>
2018-04-05 14:18:16 +0530

    make it better

--- truffle.js
+++ truffle.js
@@ -3,8 +3,9 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 require('babel-register');
 require('babel-polyfill');
 module.exports = {
+  build :"npm run dev",
   networks: {
-    development: {
+   development: { 
       host: "127.0.0.1",
       port: 9545,
       network_id: "*" // Match any network id
