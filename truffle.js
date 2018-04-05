e0d8c62303c24234e24095fee75b2f28ff5c762b
febin <febin56@gmail.com>
2018-04-05 10:47:00 +0530

    Added a Procfile

---
+++ truffle.js
@@ -0,0 +1,19 @@
+const mnemonic = process.env.ethereum_mnemonic = "finish nothing ribbon today scout tobacco ivory staff quote wasp lecture spend";
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('babel-register');
+require('babel-polyfill');
+module.exports = {
+  build: "npm run dev",
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/EtNq2cv60m36VxoZxwpo"),
+      network_id: 3,
+      gas: 4700000
+    }
+  }
+};
